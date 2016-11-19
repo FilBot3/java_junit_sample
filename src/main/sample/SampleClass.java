8448949beb6c4ce7268725aaa9efab6138b39493
Phillip Dudley <Predatorian3@gmail.com>
2016-11-18 23:57:43 -0600

    Added a VERSION file, and updated the build.xml

--- src/main/sample/SampleClass.java
+++ src/main/sample/SampleClass.java
@@ -1,20 +1,61 @@
+/**
+ * Setting the main package package name.
+ *  Just for organization of code. 
+ */
 package sample;
 
 /**
- *
+ * Import a custom library from inside the code repository. 
+ * Make sure to use the full path of packages. 
  */
+import sample.version.Version;
 
-public class SampleClass {
+/**
+ * A sample class to inherit methods from in the main class. 
+ * There can only be one main function and class per application, and class file. 
+ */
+class Dog {
+    /**
+     * print_words displays text on the screen.
+     * 
+     * @param none
+     * @return none
+     */
+    public void print_words() {
+        System.out.println("Inherited Class");
+    }
+}
+
+/** 
+ * Entry point into the application. 
+ * This is the main file and class for the application.
+ * The JAR and Classes will be ran from here. 
+ */
+public class SampleClass extends Dog {
+    /**
+     * print_words displays text on the screen.
+     * 
+     * @param none
+     * @return String (Hello World!)
+     */
     public static String print_hello() {
         System.out.println("Hello World!");
         String string_to_return = "Hello World!";
         return string_to_return;
     }
-    /**
-     *
+
+    /** 
+     * Main, the entry point for the Application
+     * 
+     * @param none
+     * @return none
      */
     public static void main(String[] args) {
         print_hello();
+        SampleClass sc = new SampleClass();
+        sc.print_words();
+        Version ver = new Version();
+        ver.VERSION();
     }
 }
 
---
+++ src/main/version/Version.java
@@ -0,0 +1,22 @@
+/** 
+ * This package is used for vresioning, and package management. 
+ * also a display of how to import a package into a main class.
+ * When distributing this application, increment the version number
+ * using Semantic Versioning. 
+ */
+
+package sample.version;
+
+/** 
+ * Contains the version information for the application.
+ * 
+ * @author Phillip Dudley
+ * @version 0.1.0
+ * @since 2016-11-18
+ */
+public class Version {
+	public void VERSION() {
+		String version = "0.1.0";
+		System.out.println(version);
+	}
+}
